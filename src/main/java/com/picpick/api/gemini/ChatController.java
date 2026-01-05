92dc3837a602c3c6c52b7fb3f0caac04931741ad
정성국 <ssoup0224@gmail.com>
2026-01-05 17:54:35 +0900

    removed unused code

--- src/main/java/com/picpick/api/gemini/ChatController.java
+++ src/main/java/com/picpick/api/gemini/ChatController.java
@@ -6,13 +6,11 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import reactor.core.publisher.Flux;
 
 @RestController
 @RequestMapping("/gemini")
 @RequiredArgsConstructor
 public class ChatController {
-    private final ChatService chatService;
     private final AnalysisService analysisService;
 
     @PostMapping("/analyze")
